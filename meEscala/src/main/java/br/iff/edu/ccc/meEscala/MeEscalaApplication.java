4300373e5589403454eeac48e7e1473773b4c64b
pedrolucaspv <pedrolucas.pv@gmail.com>
2026-06-22 21:19:04 -0300

    Preparacao Ambiente - criacao da estrutura do spring

---
+++ meEscala/src/main/java/br/iff/edu/ccc/meEscala/MeEscalaApplication.java
@@ -0,0 +1,13 @@
+package br.iff.edu.ccc.meEscala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeEscalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeEscalaApplication.class, args);
+	}
+
+}
